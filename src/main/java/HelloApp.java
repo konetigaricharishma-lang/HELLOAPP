0cde2a94600ee2fb2e3f02a6b4f8607797931c63
unknown <konetigaricharishma.email@gmail.com>
2026-03-31 18:03:54 +0530

    Coded UC6: Display Hello with multiple args using substring to remove trailing delimiter

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,14 +1,10 @@
 public class HelloApp {
 
     public static void main(String[] args) {
-
-        // Check if no arguments are passed
         if (args.length == 0) {
             System.out.println("Hello, World!");
             return;
         }
-
-        // Create StringBuilder to store names
         StringBuilder nameBuilder = new StringBuilder();
         for (String name : args) {
             nameBuilder.append(name).append(", ");
